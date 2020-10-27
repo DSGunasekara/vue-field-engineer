d881d14761a7bade3f3b441303342a41c140602d
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-10-27 23:26:45 +0530

    engineer list updated

--- client/src/views/Engineers.vue
+++ client/src/views/Engineers.vue
@@ -10,12 +10,31 @@
         v-for="engineer in engineers"
         :key="engineer.name"
       >
-        <v-card text class="text-xs-center ma-3">
+        <v-card text class="ma-3">
+          <v-responsive class="pt-4 mx-4">
+            <v-avatar center size="100" class="grey lighten-2">
+              <img src="../assets/profile.jpg" />
+            </v-avatar>
+          </v-responsive>
           <v-card-text>
-            <div class="subheading">{{ engineer.name }}</div>
-            <div class="grey--text">{{ engineer.location }}</div>
-            <div class="grey--text">{{ engineer.rating }}</div>
-            <div class="grey--text">{{ engineer.availability }}</div>
+            <div class="subheading">
+              {{ engineer.name }}
+            </div>
+            <div class="grey--text">
+              <v-icon small left>mdi-map-marker</v-icon>
+              {{ engineer.location }}
+            </div>
+            <div class="grey--text">
+              <v-icon small left>mdi-star</v-icon>
+              {{ engineer.rating }}
+            </div>
+            <div :class="`${engineer.availability} `">
+              <v-icon v-if="engineer.availability === 'available'" small left
+                >mdi-check</v-icon
+              >
+              <v-icon v-else small left>mdi-window-close</v-icon>
+              {{ engineer.availability }}
+            </div>
           </v-card-text>
           <v-card-actions>
             <v-btn text color="grey">
@@ -35,32 +54,32 @@ export default {
     return {
       engineers: [
         {
-          name: "Dilain",
-          location: "Galle",
+          name: "Dilain Gunasekara",
+          location: "Galle, Sri Lanka",
           rating: "4.1",
           availability: "available",
         },
         {
-          name: "Gunasekara",
-          location: "Colombo",
+          name: "John Cena",
+          location: "Colombo, Sri Lanka",
           rating: "4.1",
           availability: "unavailable",
         },
         {
-          name: "Jon",
-          location: "London",
+          name: "Peter Parker",
+          location: "London, Sri Lanka",
           rating: "3.1",
           availability: "available",
         },
         {
-          name: "Cena",
-          location: "Kandy",
+          name: "Tony Start",
+          location: "Kandy, Sri Lanka",
           rating: "4.8",
           availability: "unavailable",
         },
         {
-          name: "Mike",
-          location: "DC",
+          name: "The Undertaker",
+          location: "DC, USA",
           rating: "4.3",
           availability: "available",
         },
@@ -71,4 +90,10 @@ export default {
 </script>
 
 <style>
+.available {
+  color: #00c853;
+}
+.unavailable {
+  color: #e53935;
+}
 </style>
