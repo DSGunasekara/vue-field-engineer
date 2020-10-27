c24771844ebe927898aca8287f49317eb33e5cb1
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-10-27 08:21:27 +0530

    vue added

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Login/>
+  </v-app>
+</template>
+
+<script>
+
+import Login from "./components/Login";
+
+export default {
+
+  
+  name: 'App',
+
+  components: {
+    Login
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
