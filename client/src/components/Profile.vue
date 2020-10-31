807c377b968b6430260c39887a2e42b13406d129
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-10-31 10:55:56 +0530

    user profile edit added

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,22 +1,26 @@
 <template>
   <v-container>
-            <h1 class="subheading grey--text">Profile{{ getProfile}}</h1>
+            <h1 class="subheading grey--text">Profile</h1>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <UserDetails/>
+                </v-col>
+                <v-col cols="12" md="8">
+                    <v-card >Job list</v-card>
+                </v-col>
+            </v-row>
+            
   </v-container>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex"
+
+import UserDetails from './UserDetails'
 
 export default {
     name:"Profile",
-    methods:{
-        ...mapActions(['getUser'])
-    },
-    computed:{
-        ...mapGetters(["getProfile"])
-    },
-    created(){
-        this.getUser()
+    components:{
+        UserDetails
     }
 }
 </script>
---
+++ client/src/components/UserDetails.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-card>
+        <v-list-item>
+            <v-list-item-content class="grey--text headline ma-2">
+                <v-list-item-title class="headline ma-2">
+                    <v-icon>mdi-account</v-icon>
+                    {{ getProfile.name }}
+                    <span justify>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on }">
+                                <v-btn text class="grey--text" v-on="on">
+                                    <v-icon>mdi-account-edit</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Edit your profile</span>
+                        </v-tooltip>
+                    </span>
+                </v-list-item-title>
+                <v-list-item-title class="ma-2">
+                    <v-icon>mdi-email</v-icon>
+                    {{ getProfile.email }}
+                </v-list-item-title>
+                <v-list-item-title class="ma-2">
+                    <v-icon>mdi-passport</v-icon>
+                    {{ getProfile.passportNo }}
+                </v-list-item-title>
+                <v-list-item-title class="ma-2">
+                    <v-icon>mdi-cellphone</v-icon>
+                    {{ getProfile.contactNo }}
+                </v-list-item-title>
+                <v-list-item-title class="ma-2">
+                    <v-icon>mdi-map-marker</v-icon>
+                    {{ getProfile.state }}, {{ getProfile.country }}
+                </v-list-item-title>
+
+                <v-list-item-title class="ma-2">
+                    <v-icon>mdi-briefcase</v-icon>
+                    {{ getProfile.role }}
+                </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex"
+
+export default {
+    name:"UserDetails",
+    methods:{
+        ...mapActions(['getUser'])
+    },
+    computed:{
+        ...mapGetters(["getProfile"])
+    },
+    created(){
+        this.getUser()
+    }
+}
+</script>
+
+<style>
+
+</style>
