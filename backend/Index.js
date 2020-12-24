9a914368a9afd7973b5f99884d5df7aefac64601
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-12-24 14:21:49 +0530

    added image upload config

--- backend/Index.js
+++ backend/Index.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 //routes
-const userRoute = require('./routes/user');
-const authRoute = require('./routes/auth')
-const jobRoute = require('./routes/job')
-const engineerRoute = require("./routes/engineer")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const jobRoute = require("./routes/job");
+const engineerRoute = require("./routes/engineer");
 
 const app = express();
 
 //Database connection
 //Use this function to connect to database always
 connectDB().then(
-    (res)=> console.log('MongoDB Connected....'),
-    (error)=> console.log(error)
+  (res) => console.log("MongoDB Connected...."),
+  (error) => console.log(error)
 );
 
 //Init middleware
 app.use(cors());
 app.use(express.json({ extended: false }));
+app.use(express.static("./public"));
 
 //change this to redirect requests to landing page
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
-app.use('/api/user', userRoute);
-app.use('/api/login', authRoute)
-app.use('/api/job', jobRoute)
-app.use('/api/engineer', engineerRoute)
+app.use("/api/user", userRoute);
+app.use("/api/login", authRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/engineer", engineerRoute);
 
 const PORT = process.env.PORT || 5000;
 
--- backend/routes/job.js
+++ backend/routes/job.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const Job = require("../models/Job");
 const Engineer = require("../models/Engineer");
-const User = require("../models/User")
+const User = require("../models/User");
+const upload = require("../middleware/images");
+const auth = require("../middleware/verify");
+const { findById } = require("../models/Job");
 
 //get all jobs
 router.get("/", async (req, res) => {
@@ -9,10 +12,10 @@ router.get("/", async (req, res) => {
     await Job.find({})
       .populate({
         path: "assignedEngineers",
-        populate:{
+        populate: {
           path: "user",
-          select: "-password"
-        }
+          select: "-password",
+        },
       })
       .exec()
       .then((jobs, error) => {
@@ -65,8 +68,8 @@ router.patch("/:id", async (req, res) => {
   try {
     const job = await Job.findOne({ _id: req.params.id });
     if (!job) return res.status(404).send("Job does not exits");
-    if(job.requiredEngineers <= job.assignedEngineers.length)
-    await Job.updateOne({ _id: req.params.id }, req.body);
+    if (job.requiredEngineers <= job.assignedEngineers.length)
+      await Job.updateOne({ _id: req.params.id }, req.body);
     //TODO: update engineer job list history
     return res.status(200).send("Job updated");
   } catch (error) {
@@ -74,88 +77,117 @@ router.patch("/:id", async (req, res) => {
   }
 });
 
-//add a engineer to the job
-router.patch("/addEngineer/:id", async (req, res)=>{
-  try{
-    const job = await Job.findOne({ _id: req.params.id});
-    if(!job){
-      console.log('job')
-      return res.status(404).send("Job does not exits")
+//upload images
+router.patch(
+  "/upload/image",
+  auth,
+  upload.single("jobImg"),
+  async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id);
+      if (!user) return res.status(404).send("User does not exits");
+      console.log(user._id);
+      const engineer = await Engineer.findOne({ user: user._id });
+      if (!engineer) return res.status(404).send("Engineer does not exits");
+
+      return res.status(200).send(engineer);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
     }
-
-    const user = await User.findOne({ _id: req.body.engineer})
-    if(!user){
-      console.log('user')
-      return res.status(404).send("No user found")
-    }
-
-    const engineer = await Engineer.findOne({ user: user._id})
-    if(!engineer){
-      console.log('engineer')
-      return res.status(404).send("Engineer does not exits")
-    }
-
-    const jobDate = job.date.toISOString().substr(0,10)
-    const endDate = new Date(jobDate)
-    let day = jobDate.substr(8,10)
-    console.log(day)
-    endDate.setDate(parseInt(day) + 1)
-    console.log(jobDate)
-    console.log(endDate)
-
-    const jobList = await Job.find({assignedEngineers: engineer, date: {$gte: jobDate, $lt: endDate.toISOString().substr(0,10)}})
-    console.log(jobList)
-    if(jobList.length !== 0 ){
-      console.log(jobList.length)
-      return res.status(409).send("Engineer job date conflict")
-    }
-
-
-    if(engineer.availability === false) return res.status(400).send("Engineer availability is set to unavailable")
-
-    const checkEngineer = await Job.findOne({assignedEngineers: req.body.engineer})
-    if(checkEngineer) return res.status(409).send("Engineer already assigned")
-
-    if(job.requiredEngineers > job.assignedEngineers.length){
-      console.log(engineer)
-        const engineerId = engineer._id
-        job.assignedEngineers.push(engineerId)
-        if(job.requiredEngineers === job.assignedEngineers.length){
-          job.status = 'Assigned'
-        }
-        await job.save()
-        return res.status(200).send("Engineer added to the job")
-    }else {
-      return res.status(400).send("Engineer required amount is full")
-    }
-  }catch (error){
-    console.log(error)
-    return res.status(500).send(error)
   }
-})
+);
+
+//add a engineer to the job
+router.patch("/addEngineer/:id", async (req, res) => {
+  try {
+    const job = await Job.findOne({ _id: req.params.id });
+    if (!job) {
+      console.log("job");
+      return res.status(404).send("Job does not exits");
+    }
+
+    const user = await User.findOne({ _id: req.body.engineer });
+    if (!user) {
+      console.log("user");
+      return res.status(404).send("No user found");
+    }
+
+    const engineer = await Engineer.findOne({ user: user._id });
+    if (!engineer) {
+      console.log("engineer");
+      return res.status(404).send("Engineer does not exits");
+    }
+
+    const jobDate = job.date.toISOString().substr(0, 10);
+    const endDate = new Date(jobDate);
+    let day = jobDate.substr(8, 10);
+    console.log(day);
+    endDate.setDate(parseInt(day) + 1);
+    console.log(jobDate);
+    console.log(endDate);
+
+    const jobList = await Job.find({
+      assignedEngineers: engineer,
+      date: { $gte: jobDate, $lt: endDate.toISOString().substr(0, 10) },
+    });
+    console.log(jobList);
+    if (jobList.length !== 0) {
+      console.log(jobList.length);
+      return res.status(409).send("Engineer job date conflict");
+    }
+
+    if (engineer.availability === false)
+      return res
+        .status(400)
+        .send("Engineer availability is set to unavailable");
+
+    const checkEngineer = await Job.findOne({
+      assignedEngineers: req.body.engineer,
+    });
+    if (checkEngineer) return res.status(409).send("Engineer already assigned");
+
+    if (job.requiredEngineers > job.assignedEngineers.length) {
+      console.log(engineer);
+      const engineerId = engineer._id;
+      job.assignedEngineers.push(engineerId);
+      if (job.requiredEngineers === job.assignedEngineers.length) {
+        job.status = "Assigned";
+      }
+      await job.save();
+      return res.status(200).send("Engineer added to the job");
+    } else {
+      return res.status(400).send("Engineer required amount is full");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
 
 //remove an engineer from a job
-router.patch("/removeEngineer/:id", async (req, res)=>{
+router.patch("/removeEngineer/:id", async (req, res) => {
   try {
-    const job = await Job.findOne({_id: req.params.id})
-    if(!job) return res.status(404).send("Job does not exits")
+    const job = await Job.findOne({ _id: req.params.id });
+    if (!job) return res.status(404).send("Job does not exits");
 
-    const engineer = await Engineer.findOne({_id: req.body.engineer})
-    if(!engineer) return res.status(404).send("Engineer does not exits")
+    const engineer = await Engineer.findOne({ _id: req.body.engineer });
+    if (!engineer) return res.status(404).send("Engineer does not exits");
 
     //Removes the engineers from the array
-    await Job.updateOne( {_id: req.params.id}, { $pullAll: {assignedEngineers: [req.body.engineer] } } )
-    job.status = 'Pending'
-    await job.save()
+    await Job.updateOne(
+      { _id: req.params.id },
+      { $pullAll: { assignedEngineers: [req.body.engineer] } }
+    );
+    job.status = "Pending";
+    await job.save();
 
-    return res.status(200).send("Job was updated")
-
-  }catch (error){
-    console.log(error)
-    return res.status(500).send(error)
+    return res.status(200).send("Job was updated");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
   }
-})
-
+});
 
 //delete a job
 router.delete("/:id", async (req, res) => {
